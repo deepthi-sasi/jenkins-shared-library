5c378d22db42d4b7ac9748be176d502c6c0d77e5
deepthi <dpthisas@gmail.com>
2026-03-27 13:29:23 +0000

    pass parameters in buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-def call() {
+def call(String imagename) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t deepthisasi/demo-app:jma-2.2 .'
+        sh 'docker build -t $imagename .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push deepthisasi/demo-app:jma-2.2'
+        sh 'docker push $imagename'
     }
 }
